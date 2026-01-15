e70f84c7f18d3fcbb1430657e4db37dbc5772191
suhaibkhan <johnxuhaib@gmail.com>
2026-01-15 15:23:35 +0530

    url

--- 1.1-easy/src/App.jsx
+++ 1.1-easy/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     async function fetchData() {
-      const response = await fetch("");
+      const response = await fetch("https://jsonplaceholder.typicode.com/users");
       const data = await response.json();
       console.log(data)
       setUser(data)
